d87ecdab4ad71cc2c4299d4e3c033b1249ba7d6d
swaroop_a.s <swaroopanand325@gmail.com>
2021-02-04 21:33:39 +0530

    response returned

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -25,7 +25,7 @@ exports.register = async (req, res) => {
       .status(200)
       .json({ success: true, message: "Account created successfully" });
   } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
+    res.status(200).json({ success: false, message: error.message });
   }
 };
 
@@ -48,11 +48,11 @@ exports.login = async (req, res) => {
 
     //validate password
     if (!user.comparePassword(password))
-      return res.status(401).json({ message: "Invalid email or password" });
+      return res.status(200).json({ message: "Invalid email or password" });
 
     // Login successful, write token, and send back user
     res.status(200).json({ token: user.generateJWT(), user: user });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(200).json({ message: error.message });
   }
 };
